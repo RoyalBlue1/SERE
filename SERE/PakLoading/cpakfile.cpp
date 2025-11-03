c73c609df13f5fec84e4825bb6e597993b12159d
RoyalBlue1 <11448698+RoyalBlue1@users.noreply.github.com>
2025-11-03 20:31:54 +0100

    Allow Int Input to all applicable Float inputs Adds origin to PakLoading Fix Bug with device creation

--- SERE/PakLoading/cpakfile.cpp
+++ SERE/PakLoading/cpakfile.cpp
@@ -1,3 +1,4 @@
+//from github.com/r-ex/rsx
 
 #include "PakLoading/cpakfile.h"
 #include "PakLoading/patchapi.h"
--- SERE/RuiNodeEditor/RuiBaseNode.cpp
+++ SERE/RuiNodeEditor/RuiBaseNode.cpp
@@ -15,3 +15,19 @@ std::string Variable::GetFormattedName(RuiExportPrototype& proto) const {
 		return std::format("data->{}",name);
 	return name;
 }
+
+FloatVariable RuiBaseNode::getInNumeric(const char* id) {
+	float val;
+	if(inPin(id)->isConnected()&&inPin(id)->getLink().lock()->left()->getDataType() == typeid(IntVariable))
+	{
+		IntVariable val = getInVal<IntVariable>(id).value;
+		return FloatVariable(val.value,val.name);
+	}
+	else
+		return getInVal<FloatVariable>(id).value;
+
+}
+
+bool isPinNumeric(const std::type_info& out, const std::type_info& in) {
+	return (out == typeid(IntVariable))||(out == typeid(FloatVariable));
+}
