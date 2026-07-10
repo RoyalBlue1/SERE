fe68c539373fe5f67d9449f2acb3c7f64f08bcc3
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2026-07-10 13:54:30 -0700

    add printf node

--- SERE/Nodes/FunctionNodes.cpp
+++ SERE/Nodes/FunctionNodes.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cctype>
 #include <format>
+#include <type_traits>
 #include <variant>
 #include "Imgui/imgui_stdlib.h"
 
@@ -248,6 +249,75 @@ std::string GetFormattedLocalizeArgument(const LocalizeArgument& argument, RuiEx
 std::string GetLocalizeArgumentName(const LocalizeArgument& argument)
 {
 	return std::visit([](const auto& value) { return value.name; }, argument);
+}
+
+static char IntegerPresentation(const std::string& options)
+{
+	if (options.size() < 3 || options.front() != '{' || options.back() != '}')
+		return '\0';
+	for (auto it = options.rbegin() + 1; it != options.rend(); ++it) {
+		if (*it == 'd' || *it == 'x' || *it == 'X' || *it == 'o' ||
+			*it == 'b' || *it == 'B' || *it == 'c')
+			return *it;
+		if ((*it >= 'a' && *it <= 'z') || (*it >= 'A' && *it <= 'Z'))
+			break;
+	}
+	return '\0';
+}
+
+static std::string PrintfIntegerSpecifier(const std::string& options, char presentation)
+{
+	std::string modifiers;
+	const size_t colon = options.find(':');
+	if (colon != std::string::npos && options.size() > colon + 2) {
+		const size_t presentationIndex = options.size() - 2;
+		for (size_t index = colon + 1; index < presentationIndex; ++index) {
+			const char character = options[index];
+			if ((character >= '0' && character <= '9') || character == '+' ||
+				character == '-' || character == ' ' || character == '#')
+				modifiers += character;
+		}
+	}
+
+	std::string specifier = "%" + modifiers;
+	switch (presentation) {
+	case 'x': return specifier + 'x';
+	case 'X': return specifier + 'X';
+	case 'o': return specifier + 'o';
+	case 'c': return specifier + 'c';
+	default: return specifier + 'i';
+	}
+}
+
+static char FloatPresentation(const std::string& options)
+{
+	if (options.size() < 3 || options.front() != '{' || options.back() != '}')
+		return '\0';
+	const char presentation = options[options.size() - 2];
+	switch (presentation) {
+	case 'a': case 'A': case 'e': case 'E':
+	case 'f': case 'F': case 'g': case 'G':
+		return presentation;
+	default:
+		return '\0';
+	}
+}
+
+static std::string PrintfFloatSpecifier(const std::string& options)
+{
+	const char presentation = FloatPresentation(options);
+	std::string modifiers;
+	const size_t colon = options.find(':');
+	if (colon != std::string::npos) {
+		const size_t end = presentation == '\0' ? options.size() - 1 : options.size() - 2;
+		for (size_t index = colon + 1; index < end; ++index) {
+			const char character = options[index];
+			if ((character >= '0' && character <= '9') || character == '.' ||
+				character == '+' || character == '-' || character == ' ' || character == '#')
+				modifiers += character;
+		}
+	}
+	return "%" + modifiers + (presentation == '\0' ? 'f' : presentation);
 }
 
 LocalizeNode::LocalizeNode(RenderInstance& rend, ImFlow::StyleManager& style) :RuiBaseNode(name, category, GetPinInfo(), rend, style) {
@@ -512,6 +582,261 @@ std::vector<std::shared_ptr<ImFlow::PinProto>> LocalizeNode::GetPinInfo() {
 	return info;
 }
 
+PrintFNode::PrintFNode(RenderInstance& rend, ImFlow::StyleManager& style)
+	: RuiBaseNode(name, category, GetPinInfo(), rend, style)
+{
+	const std::string outName = Variable::UniqueName();
+	getOut<StringVariable>("Out")->behaviour([this, outName]() {
+		std::string value;
+		Print(value);
+		return StringVariable(value, outName);
+		});
+	AddArgumentPin();
+}
+
+PrintFNode::PrintFNode(RenderInstance& rend, ImFlow::StyleManager& style,
+	rapidjson::GenericObject<false, rapidjson::Value> obj)
+	: RuiBaseNode(name, category, GetPinInfo(), rend, style)
+{
+	const std::string outName = Variable::UniqueName();
+	getOut<StringVariable>("Out")->behaviour([this, outName]() {
+		std::string value;
+		Print(value);
+		return StringVariable(value, outName);
+		});
+
+	if (obj.HasMember("Format") && obj["Format"].IsString())
+		fmt = obj["Format"].GetString();
+
+	size_t savedArgumentCount = 1;
+	if (obj.HasMember("ArgumentCount") && obj["ArgumentCount"].IsUint64())
+		savedArgumentCount = static_cast<size_t>(obj["ArgumentCount"].GetUint64());
+	for (size_t index = 0; index < savedArgumentCount; ++index)
+		AddArgumentPin();
+}
+
+void PrintFNode::AddArgumentPin()
+{
+	static std::function<bool(const std::type_info&, const std::type_info&)> printfArgument =
+		[](const std::type_info& out, const std::type_info&) {
+			return out == typeid(BoolVariable) ||
+				out == typeid(IntVariable) ||
+				out == typeid(FloatVariable) ||
+				out == typeid(StringVariable);
+		};
+
+	++argumentCount;
+	auto pin = std::make_shared<ImFlow::InPinProto<StringVariable>>(
+		std::format("Arg {}", argumentCount), printfArgument, StringVariable(""));
+	pin->CreatePin(this, styles);
+}
+
+std::string PrintFNode::FormatArgument(size_t index, const std::string& options)
+{
+	const std::string id = std::format("Arg {}", index);
+	if (!inPin(id)->isConnected())
+		return "***";
+
+	try {
+		return std::visit([&options](const auto& value) {
+			if constexpr (std::is_same_v<std::decay_t<decltype(value)>, FloatVariable>) {
+				if (IntegerPresentation(options) != '\0') {
+					int converted = static_cast<int>(value.value);
+					return std::vformat(options, std::make_format_args(converted));
+				}
+			}
+			return std::vformat(options, std::make_format_args(value.value));
+			}, GetLocalizeArgument(*this, id));
+	}
+	catch (const std::format_error&) {
+		return "***";
+	}
+}
+
+bool PrintFNode::Print(std::string& out)
+{
+	size_t argumentIndex = 1;
+	for (size_t index = 0; index < fmt.size(); ++index) {
+		if (fmt[index] == '{') {
+			if (index + 1 >= fmt.size()) {
+				out = "***";
+				return false;
+			}
+			if (fmt[index + 1] == '{') {
+				out += '{';
+				++index;
+				continue;
+			}
+
+			std::string options;
+			bool closed = false;
+			for (; index < fmt.size(); ++index) {
+				options += fmt[index];
+				if (fmt[index] == '}') {
+					closed = true;
+					break;
+				}
+			}
+			if (!closed || argumentIndex > argumentCount) {
+				out = "***";
+				return false;
+			}
+			out += FormatArgument(argumentIndex++, options);
+		}
+		else if (fmt[index] == '}') {
+			if (index + 1 < fmt.size() && fmt[index + 1] == '}') {
+				out += '}';
+				++index;
+			}
+			else {
+				out = "***";
+				return false;
+			}
+		}
+		else {
+			out += fmt[index];
+		}
+	}
+	return true;
+}
+
+int PrintFNode::GetPrintfString(std::string& out, std::vector<bool>* floatArgumentsAsInt)
+{
+	if (floatArgumentsAsInt)
+		floatArgumentsAsInt->clear();
+	size_t argumentIndex = 1;
+	for (size_t index = 0; index < fmt.size(); ++index) {
+		if (fmt[index] == '{') {
+			if (index + 1 >= fmt.size())
+				return -1;
+			if (fmt[index + 1] == '{') {
+				out += '{';
+				++index;
+				continue;
+			}
+
+			std::string options;
+			bool closed = false;
+			for (; index < fmt.size(); ++index) {
+				options += fmt[index];
+				if (fmt[index] == '}') {
+					closed = true;
+					break;
+				}
+			}
+			if (!closed || argumentIndex > argumentCount)
+				return -1;
+
+			ImFlow::Pin* input = inPin(std::format("Arg {}", argumentIndex++));
+			const char integerPresentation = IntegerPresentation(options);
+			const bool floatAsInt = input->isConnected() &&
+				input->getLink().lock()->left()->getDataType() == typeid(FloatVariable) &&
+				integerPresentation != '\0';
+			if (floatArgumentsAsInt)
+				floatArgumentsAsInt->push_back(floatAsInt);
+			if (!input->isConnected() || input->getLink().lock()->left()->getDataType() == typeid(StringVariable))
+				out += "%s";
+			else if (integerPresentation != '\0')
+				out += PrintfIntegerSpecifier(options, integerPresentation);
+			else if (input->getLink().lock()->left()->getDataType() == typeid(FloatVariable))
+				out += PrintfFloatSpecifier(options);
+			else
+				out += "%i";
+		}
+		else if (fmt[index] == '}') {
+			if (index + 1 >= fmt.size() || fmt[index + 1] != '}')
+				return -1;
+			out += '}';
+			++index;
+		}
+		else if (fmt[index] == '%') {
+			out += "%%";
+		}
+		else {
+			out += fmt[index];
+		}
+	}
+	return static_cast<int>(argumentIndex - 1);
+}
+
+void PrintFNode::draw()
+{
+	ImGui::PushItemWidth(160.f);
+	ImGui::InputText("Format", &fmt);
+	ImGui::PopItemWidth();
+
+	const bool canRemove = argumentCount > 0 &&
+		!inPin(std::format("Arg {}", argumentCount))->isConnected();
+	ImGui::BeginDisabled(!canRemove);
+	if (ImGui::SmallButton("-")) {
+		const std::string id = std::format("Arg {}", argumentCount);
+		dropIN(id);
+		--argumentCount;
+	}
+	ImGui::EndDisabled();
+	ImGui::SameLine();
+	if (ImGui::SmallButton("+"))
+		AddArgumentPin();
+
+	std::string preview;
+	Print(preview);
+	ImGui::Text("\"%s\"", preview.c_str());
+}
+
+void PrintFNode::Serialize(rapidjson::GenericValue<rapidjson::UTF8<>>& obj,
+	rapidjson::Document::AllocatorType& allocator)
+{
+	obj.AddMember("Name", name, allocator);
+	obj.AddMember("Category", category, allocator);
+	obj.AddMember("Format", fmt, allocator);
+	obj.AddMember("ArgumentCount", static_cast<uint64_t>(argumentCount), allocator);
+	RuiBaseNode::Serialize(obj, allocator);
+}
+
+void PrintFNode::Export(RuiExportPrototype& proto)
+{
+	const StringVariable out = getOut<StringVariable>("Out")->val();
+	std::string printfString;
+	std::vector<bool> floatArgumentsAsInt;
+	const int usedArgumentCount = GetPrintfString(printfString, &floatArgumentsAsInt);
+	if (usedArgumentCount < 0)
+		return;
+
+	std::vector<LocalizeArgument> arguments;
+	arguments.reserve(usedArgumentCount);
+	for (int index = 1; index <= usedArgumentCount; ++index)
+		arguments.push_back(GetLocalizeArgument(*this, std::format("Arg {}", index)));
+
+	ExportElement<std::string> element;
+#if _DEBUG
+	element.sourceNodeName = typeid(*this).name();
+#endif
+	element.identifier = out.name;
+	for (const LocalizeArgument& argument : arguments)
+		element.dependencys.insert(GetLocalizeArgumentName(argument));
+	element.callback = [out, printfString, arguments, floatArgumentsAsInt](RuiExportPrototype& proto) {
+		const StringVariable format(printfString);
+		std::string call = std::format("funcs->printf(inst, {}", format.GetFormattedName(proto));
+		for (size_t index = 0; index < arguments.size(); ++index) {
+			std::string value = GetFormattedLocalizeArgument(arguments[index], proto);
+			if (floatArgumentsAsInt[index])
+				value = std::format("static_cast<int>({})", value);
+			call += std::format(", {}", value);
+		}
+		call += ')';
+		if (proto.varsInDataStruct.contains(out.name))
+			proto.codeLines.push_back(std::format("{} = {};", out.GetFormattedName(proto), call));
+		else
+			proto.codeLines.push_back(std::format("const char* {} = {};", out.GetFormattedName(proto), call));
+		};
+	proto.codeElements.push_back(element);
+}
+
+std::vector<std::shared_ptr<ImFlow::PinProto>> PrintFNode::GetPinInfo()
+{
+	return { std::make_shared<ImFlow::OutPinProto<StringVariable>>("Out") };
+}
+
 
 void AddFunctionNodes(NodeEditor& editor)
 {
@@ -520,5 +845,6 @@ void AddFunctionNodes(NodeEditor& editor)
 	editor.AddNodeType<ProjectionNode>();
 	editor.AddNodeType<ToUpperNode>();
 	editor.AddNodeType<LocalizeNode>();
+	editor.AddNodeType<PrintFNode>();
 
 }
