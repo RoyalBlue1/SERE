702499b188ea99f27b8d47f916104dd42013a52b
RoyalBlue1 <11448698+RoyalBlue1@users.noreply.github.com>
2026-07-18 23:58:03 +0200

    Fix Windows build

--- SERE/RuiNodeEditor/RuiBasePin.h
+++ SERE/RuiNodeEditor/RuiBasePin.h
@@ -6,23 +6,71 @@
 template <>
 void ImFlow::InPin<MathVariable>::drawNodeContent();
 
+template <>
+void ImFlow::InPin<MathVariable>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<MathVariable>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
+
 template<>
 void ImFlow::InPin<FloatVariable>::drawNodeContent();
+
+template <>
+void ImFlow::InPin<FloatVariable>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<FloatVariable>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
 
 template<>
 void ImFlow::InPin<Float2Variable>::drawNodeContent();
 
+template <>
+void ImFlow::InPin<Float2Variable>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<Float2Variable>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
+
 template<>
 void ImFlow::InPin<Float3Variable>::drawNodeContent();
+
+template <>
+void ImFlow::InPin<Float3Variable>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<Float3Variable>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
 
 template<>
 void ImFlow::InPin<ColorVariable>::drawNodeContent();
 
+template <>
+void ImFlow::InPin<ColorVariable>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<ColorVariable>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
+
 template<>
 void ImFlow::InPin<TransformSize>::drawNodeContent();
+
+template <>
+void ImFlow::InPin<TransformSize>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<TransformSize>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
 
 template<>
 void ImFlow::InPin<AssetVariable>::drawNodeContent();
 
+template <>
+void ImFlow::InPin<AssetVariable>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<AssetVariable>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
+
 template<>
 void ImFlow::InPin<StringVariable>::drawNodeContent();
+
+template <>
+void ImFlow::InPin<StringVariable>::LoadEmptyValue(rapidjson::Value& value);
+
+template <>
+void ImFlow::InPin<StringVariable>::StoreEmptyValue(rapidjson::GenericValue<rapidjson::UTF8<>>& object, rapidjson::Document::AllocatorType& allocator);
